28e9ee169e80ea75e9b6eb785c867f074ddf5dee
1294167955 <1294167955@qq.com>
2019-10-09 11:31:08 +0800

    Lx06

---
+++ DAL/Comment.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    public class Comment
+    {
+        private Comment() { }
+        private static Comment _instance = new Comment();
+        public static Comment Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        string cns = AppConfigurtaionServices.Configuration.GetConnectionString("cns");
+        public Model.Comment GetModel(int id)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select * from Comment where commentId=@id";
+                return cn.QueryFirstOrDefault<Model.Comment>(sql, new { id = id });
+            }
+        }
+        public IEnumerable<Model.Comment> GetAll()
+        {
+            using(IDbConnection cn=new MySqlConnection(cns))
+            {
+                string sql = "select * from Comment";
+                return cn.Query<Model.Comment>(sql);
+            }
+        }
+        public int GetCount()
+        {
+            using (IDbConnection cn=new MySqlConnection(cns))
+            {
+                string sql = "select count(1) from Comment";
+                return cn.ExecuteScalar<int>(sql);
+            }
+        }
+        public IEnumerable<Model.CommentNo> GetPage(Model.Page page)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "with a as(select row_number() over(order by commentTime desc) as num, Comment.*,workName from Comment join workinfo on Comment.workId=workinfo.workId)";
+                sql += "select * from a where num between (@pageIndex-1)*@pageSize+1 and @pageIndex*@pageSize";
+                return cn.Query<Model.CommentNo>(sql,page);
+            }
+        }
+        public int GetWorkCount(int id)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select count(1) from Comment where workId=@workId";
+                return cn.ExecuteScalar<int>(sql,new { workId=id});
+            }
+        }
+        public IEnumerable<Model.CommentNo> GetWorkPage(Model.CommentPage page)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "with a as(select row_number() over(order by commentTime desc) as num, Comment.* from Comment where Comment.workId=@workId)";
+                sql += "select* from a where num between (@pageIndex-1)*@pageSize+1 and @pageIndex*@pageSize;";
+                return cn.Query<Model.CommentNo>(sql, page);
+            }
+        }
+        public int Add(Model.Comment comment)
+        {
+            using(IDbConnection cn=new MySqlConnection(cns))
+            {
+                string sql = "insert into Comment(commentID,workId,userName,CommentContent,CommentTime)" + "values(@commentID,@workId,@userName,@commentContent,@commentTime);";
+                sql += "SELECT @@IDENTITY";
+                return cn.ExecuteScalar<int>(sql, comment);
+            }
+        }
+        public int Update(Model.Comment comment)
+        {
+            using(IDbConnection cn=new MySqlConnection(cns))
+            {
+                string sql = "update Comment set CommentContent=@CommentContent,CommentTime=@CommentContent where commentID=@commentID";
+                return cn.Execute(sql, comment);
+            }
+        }
+        public int Delete(int id)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "delete from comment where commentId=@id";
+                return cn.Execute(sql, new{id=id});
+            }
+        }
+    }
+}
---
+++ Service/Controllers/CommentController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.Comment.Instance.GetCount());
+        }
+
+        // GET: api/<controller>5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Json(DAL.Comment.Instance.GetWorkCount(id));
+        }
+
+        [HttpPost("page")]
+        public ActionResult getPage([FromBody] Model.Page page) 
+        {
+            var result = DAL.Comment.Instance.GetPage(page);
+            if (result.Count() == 0)
+                return Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+
+        [HttpPost("workPage")]
+        public ActionResult getWorkPage([FromBody] Model.CommentPage page)
+        {
+            var result = DAL.Comment.Instance.GetWorkPage(page);
+            if (result.Count() == 0)
+                return Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var n = DAL.Comment.Instance.Delete(id);
+                if (n != 0)
+                    return Json(Result.Ok("删除成功"));
+                else
+                    return Json(Result.Err("commentId不存在"));
+            }
+            catch( Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody]Model.Comment comment)
+        {
+            comment.commentTime = DateTime.Now;
+            try
+            {
+                int n = DAL.Comment.Instance.Add(comment);
+                return Json(Result.Ok("发表评论成功", n));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("foreign key"))
+                    if(ex.Message.ToLower().Contains("username"))
+                        return Json(Result.Err("合法用户才能添加记录"));
+                    else
+                         return Json(Result.Err("评论所属作品不存在"));
+                else if(ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("评论内容、作品Id、用户名不能为空"));
+                else 
+                    return Json(Result.Err(ex.Message));
+
+            }
+        }
+    }
+}
